420f975b127be01f0d1c12b7c4b7b8fa8d59bb24
Lorenzo Delana <oss.devel@searchathing.com>
2019-04-24 15:48:35 +0200

    data age test

--- material-table/test01/src/App.tsx
+++ material-table/test01/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Grid, TextField, ExpansionPanel, ExpansionPanelSummary, Typography, ExpansionPanelDetails } from "@material-ui/core";
+import { Grid, TextField, ExpansionPanel, ExpansionPanelSummary, Typography, ExpansionPanelDetails, Button } from "@material-ui/core";
 import MaterialTable, { Column } from "material-table";
 
 interface IState {
     text: string;
     colRenderCount: number;
+    dataAge: number;
 }
 
 interface IData {
@@ -22,6 +23,7 @@ export default class App extends React.Component<{}, IState> {
         this.state = {
             text: "",
             colRenderCount: 0,
+            dataAge: 0,
         }
 
         this.data = [
@@ -38,19 +40,28 @@ export default class App extends React.Component<{}, IState> {
                 direction="column"
                 justify="flex-start"
                 alignItems="flex-start"
+                spacing={24}
             >
                 <Grid item={true}>
                     <TextField label="type here" value={this.state.text} onChange={this.onTextChange} />
                 </Grid>
 
                 <Grid item={true}>
-                    column render count : {this.state.colRenderCount}
+                    <Button color="primary" variant="contained" onClick={this.addData}>data add</Button>
+                
+                    <Button color="default" variant="contained" onClick={this.incrementAge}>age increment</Button>
+                </Grid>
+
+                <Grid item={true}>
+                    RENDER (column) : {this.state.colRenderCount} |
+                    AGE (data) : {this.state.colRenderCount}
                 </Grid>
 
                 <Grid item={true}>
 
                     <MaterialTable
                         data={this.data}
+                        dataAge={this.state.dataAge}
                         columns={[
                             { field: "val", render: this.renderCol },
                         ]} />
@@ -59,6 +70,14 @@ export default class App extends React.Component<{}, IState> {
 
             </Grid>
         );
+    }
+
+    private addData = () => {
+        this.data.push({ val: this.data.length });        
+    }
+
+    private incrementAge = () => {
+        this.setState({ dataAge: this.state.dataAge + 1 });
     }
 
     private renderCol = (rowData: IData) => {
