27d466f1c584a96011814b3d8199467727ee6822
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-06-16 13:09:28 +0200

    repro with v1.39.1

--- material-table/test01/src/App.tsx
+++ material-table/test01/src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Grid, TextField, ExpansionPanel, ExpansionPanelSummary, Typography, ExpansionPanelDetails, Button } from "@material-ui/core";
-import MaterialTable, { Column } from "material-table";
+import { Grid, TextField,  Button } from "@material-ui/core";
+import MaterialTable from "material-table";
 
 interface IState {
-    text: string;
-    dataAge: number;
+    text: string;    
 }
 
 interface IData {
@@ -13,25 +12,21 @@ interface IData {
 
 export default class App extends React.Component<{}, IState> {
 
-    private data: IData[];
-    private spanRef: React.RefObject<HTMLInputElement>;
+    private data: IData[];    
     private renderColCnt: number = 0;
 
     constructor(props: {}) {
         super(props);
 
         this.state = {
-            text: "",
-            dataAge: -1,
+            text: "",            
         }
 
         this.data = [
             { val: 0 },
             { val: 1 },
             { val: 2 },
-        ];
-
-        this.spanRef = React.createRef();
+        ];        
     }
 
     public render() {
@@ -48,21 +43,13 @@ export default class App extends React.Component<{}, IState> {
                 </Grid>
 
                 <Grid item={true}>
-                    <Button color="primary" variant="contained" onClick={this.addData}>data add</Button>
-
-                    <Button color="default" variant="contained" onClick={this.incrementAge}>age increment</Button>
-                </Grid>
-
-                <Grid item={true}>
-                    Render (col) : <TextField disabled={true} inputRef={this.spanRef} />
-                    AGE (data) : {this.state.dataAge}
-                </Grid>
+                    <Button color="primary" variant="contained" onClick={this.addData}>data add</Button>                    
+                </Grid> 
 
                 <Grid item={true}>
 
                     <MaterialTable
-                        data={this.data}
-                        dataAge={this.state.dataAge}
+                        data={this.data}                        
                         columns={[
                             { field: "val", render: this.renderCol },
                         ]} />
@@ -75,14 +62,6 @@ export default class App extends React.Component<{}, IState> {
 
     private addData = () => {
         this.data.push({ val: this.data.length });
-    }
-
-    private incrementAge = () => {
-        if (this.state.dataAge === undefined) {
-            this.setState({ dataAge: 0 });
-        } else {
-            this.setState({ dataAge: this.state.dataAge + 1 });
-        }
     }
 
     private renderCol = (rowData: IData) => {
