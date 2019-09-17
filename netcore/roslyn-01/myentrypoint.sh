33a969f8f9376ef9c83a6125d690a80413aca4b1
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-09-17 22:37:48 +0200

    using runtime

--- netcore/roslyn-01/myentrypoint.sh
+++ netcore/roslyn-01/myentrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "------------ENTRYPOINT"
-export DOTNET_ROOT=/usr/share/dotnet
-export MSBuildSDKsPath=$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks
+#export DOTNET_ROOT=/usr/share/dotnet
+#export MSBuildSDKsPath=$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks
 
-dotnet --list-sdks
+dotnet --list-runtimes
 
 echo "dotnet executable = [$(which dotnet)]"
 echo "PATH              = [$PATH]"
@@ -13,5 +13,5 @@ echo "MSBuildSDKsPath   = [$MSBuildSDKsPath]"
 
 #ENV COREHOST_TRACE=1
 
+ls -la /app
 dotnet /app/analyzer.dll
-
