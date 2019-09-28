56b3b89d6e317e9ce1d592289b5bff598f326298
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-09-29 01:41:17 +0200

    fixed by settings MSBUILD_EXE_PATH

--- netcore/roslyn-01/myentrypoint.sh
+++ netcore/roslyn-01/myentrypoint.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 echo "------------ENTRYPOINT"
-#export DOTNET_ROOT=/usr/share/dotnet
+export DOTNET_ROOT=/usr/share/dotnet
 #export MSBuildSDKsPath=$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/Sdks
+
+#
+# without MSBUILD_EXE_PATH follow error results
+# Msbuild failed when processing the file '/src/test/test.csproj' with message: The SDK 'Microsoft.NET.Sdk' specified could not be found.  /src/test/test.csproj
+#
+export MSBUILD_EXE_PATH=$DOTNET_ROOT/sdk/$(${DOTNET_ROOT}/dotnet --version)/MSBuild.dll
 
 dotnet --list-runtimes
 
 echo "dotnet executable = [$(which dotnet)]"
 echo "PATH              = [$PATH]"
-#echo "DOTNET_ROOT       = [$DOTNET_ROOT]"
+echo "DOTNET_ROOT       = [$DOTNET_ROOT]"
+echo "MSBUILD_EXE_PATH  = [$MSBUILD_EXE_PATH]"
 #echo "MSBuildSDKsPath   = [$MSBuildSDKsPath]"
 
 #export COREHOST_TRACE=1
