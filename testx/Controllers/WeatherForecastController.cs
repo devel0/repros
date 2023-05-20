56019eebf1ccdce513d3217e586b778abc047db5
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-05-20 13:21:15 +0200

    repro issue https://github.com/OmniSharp/omnisharp-vscode/issues/3558 can be closed

---
+++ testx/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testx.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<WeatherForecast>> Get()
+    {
+        await Task.Run(() => { });
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast // breakpoint here ok
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
